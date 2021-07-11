e6c9efb07747e749e263625243d72d8cf7914809
Andrea Bentley <andrea@bentleytech.com>
2021-07-11 14:34:00 -0400

    Fixed active menu

--- js/header.js
+++ js/header.js
@@ -31,13 +31,13 @@ class Header extends HTMLElement {
             <div class="navbar-collapse offcanvas-collapse" id="collapseMenu">
               <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                  <a class="nav-link" id="home-link" href="index.html">Home</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="portfolio.html">Portfolio</a>
+                  <a class="nav-link" id="portfolio-link" href="portfolio.html">Portfolio</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="about.html">About</a>
+                  <a class="nav-link" id="about-link" href="about.html">About</a>
                 </li>
                 <a class="nav-link" href="mailto:JosephJohnSlatteryJr@SlatteryCompanyLLC.com">Contact</a>
                 </li>
--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 "use strict";
 
 $(function() {
-  $(".navbar-toggler")[0].addEventListener("click", function () {
+  // Set onclick handler to open/close side menu when hamburger clicked on mobile
+  $(".navbar-toggler").click(() => {
     $(".offcanvas-collapse")[0].classList.toggle("open");
   });
 
-  AOS.init({duration: 1500}); // Initialize Animate on Scroll library 
+  // Make appropriate navbar link active
+  $(".nav-item").attr("aria-current", false);
+  $(".nav-link").find(".active").removeClass("active");
+  if (window.location.pathname.includes("portfolio")) {
+    $("#portfolio-link").addClass("active");
+    $("#portfolio-link").parent().attr("aria-current", "page");
+  }
+  else if (window.location.pathname.includes("about")) {
+    $("#about-link").addClass("active");
+    $("#about-link").parent().attr("aria-current", "page");
+  }
+  else {
+    $("#home-link").addClass("active");
+    $("#home-link").parent().attr("aria-current", "page");
+  };
+
+  // Initialize Animate on Scroll library 
+  AOS.init({duration: 1500}); 
 });
